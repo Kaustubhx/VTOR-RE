72edff16640d12ae92de61d10a56c1ed4bd772f1
Kastubhx <prime.ganekar@gmail.com>
2022-10-19 19:31:12 +0530

    New: Added google maps integration

--- src/app/pages/contactpage/contactpage.component.ts
+++ src/app/pages/contactpage/contactpage.component.ts
@@ -12,4 +12,25 @@ export class ContactpageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  display: any;
+  center: google.maps.LatLngLiteral = {
+    lat: 19.2109178,
+    lng: 72.8564248
+  };
+  zoom = 20;
+  moveMap(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.center = (event.latLng.toJSON());
+  }
+  move(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
+
+  markerOptions: google.maps.MarkerOptions = {
+    draggable: false
+  };
+  markerPositions: google.maps.LatLngLiteral[] = [];
+  addMarker(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.markerPositions.push(event.latLng.toJSON());
+  }
+
 }
