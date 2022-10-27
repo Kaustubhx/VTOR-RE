fccd45a1b2e75bb84d7a9acdfba01f7bee634285
Kastubhx <prime.ganekar@gmail.com>
2022-10-27 13:26:18 +0530

    Add & Fix: Images for products & fix some styling

--- src/app/pages/aboutpage/aboutpage.component.ts
+++ src/app/pages/aboutpage/aboutpage.component.ts
@@ -11,15 +11,17 @@ export class AboutpageComponent implements OnInit {
     {
       memberAvatar: 'assets/images/teammember3.png',
       memberRole: 'Founder',
-      memberName: 'Abhijit Chaudhry',
-      memberIntro: 'He comes from a information security background with strong hold on research and design of solution',
+      memberName: 'Moxesh Mehta',
+      memberIntro: 'He comes from a product background and is an expert in product creation , trend and its propogation in market',
+      linkedIn: 'https://www.linkedin.com/in/moxesh-mehta-a64a67147/',
     },
     {
       memberAvatar: 'assets/images/teammember3.png',
       memberRole: 'Founder',
-      memberName: 'Moxesh Mehta',
-      memberIntro: 'He comes from a product background and is an expert in product creation , trend and its propogation in market',
-    }
+      memberName: 'Abhijit Chaudhry',
+      memberIntro: 'He comes from a information security background with strong hold on research and design of solution',
+      linkedIn: 'https://www.linkedin.com/in/abhijit-chaudhry-97aa9619b/',
+    },
   ]
 
   constructor() { }
